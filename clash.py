b0c4517b42adbead2ce5cf74119c19a769086054
Ian Maloney <ian_maloney@verizon.net>
2020-01-12 02:59:44 -0500

    added help statements

--- clash.py
+++ clash.py
@@ -8,70 +8,72 @@ def get_key(file):
 
 # still need to handle bad requests
 def make_request(endpoint, file):
+	if not file:
+		file = "./api_key.txt"
 	key = get_key(file)
 	header = {"Authorization": "Bearer %s" % key}
 	base_url = "https://api.clashroyale.com/v1"
 	return requests.get(base_url + endpoint, headers=header)
 
-def get_clan_info(clan_tag, file="./api_key.txt"):
+def get_clan_info(clan_tag, file):
 	endpoint = "/clan/%23" + clan_tag
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_clan_members(clan_tag, file="./api_key.txt"):
+def get_clan_members(clan_tag, file):
 	endpoint = "/clan/%23" + clan_tag + "/members"
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_clan_warlog(clan_tag, file="./api_key.txt"):
+def get_clan_warlog(clan_tag, file):
 	endpoint = "/clan/%23" + clan_tag + "/warlog"
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_clan_currentwar(clan_tag, file="./api_key.txt"):
+def get_clan_currentwar(clan_tag, file):
 	endpoint = "/clan/%23" + clan_tag + "/currentwar"
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
 '''
 Returns player info in json format
 '''
-def get_player_info(player_tag, file="./api_key.txt"):
+def get_player_info(player_tag, file):
 	endpoint = "/players/%23" + player_tag
 	# may do a little more with info
 	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_player_upcomingchests(player_tag, file="./api_key.txt"):
+def get_player_upcomingchests(player_tag, file):
 	endpoint = "/players/%23" + player_tag + "/upcomingchests"
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_player_battlelog(player_tag, file="./api_key.txt"):
+def get_player_battlelog(player_tag, file):
 	endpoint = "/players/%23" + player_tag + "/battlelog"
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_tournament_info(tournament_tag, file="./api_key.txt"):
+def get_tournament_info(tournament_tag, file):
 	endpoint = "/tournaments/%23" + tournament_tag
-	info = json.loads(make_request(endpoint).text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
 '''
 Gets list of locations --> take out of json
 '''
-def get_locations():
+def get_locations(file):
 	endpoint = "/locations"
-	info = json.loads(make_request(endpoint, file="./api_key.txt").text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
 # 57000000
-def get_locations():
+def get_locations(file):
 	endpoint = "/locations"
-	info = json.loads(make_request(endpoint, file="./api_key.txt").text)
+	info = json.loads(make_request(endpoint, file).text)
 	return info
 
-def get_cards(file="./api_key.txt"):
+def get_cards(file):
 	info = json.loads(make_request("/cards", file).text)
 	return info
 
--- main.py
+++ main.py
@@ -5,30 +5,85 @@ def quit(cmd):
 		return True
 	return False
 
-def print_help()
-	
+def print_player_help():
+	# TODO: need to add -w and -l 
+	print("\nHelp: p [player tag] [options...]\n\tPrints information about the player." +
+	 "The player tag is the set of characters\n\tfound the username in the player profile portal.\n\n" + 
+	 "\tOPTIONS:\n\t\t-l[s/d][number of matches]\t\treturn the number of battles\n\t\t\t\t\t\t\tincluding" + 
+	 " details.\n\t\t-c\t\tReturn the type of chest indexed at when the player will\n\t\t\t\trecieve the" + 
+	 "chest.\n\t\t-n\t\tReturn the name of the player.\n\t\t-lvl\t\treturn the level of the player.\n" + 
+	 "\t\t-t\t\tReturn the current trophy count of the player.\n\t\t-wl\t\tReturn the singles win loss" + 
+	 " ratio of the player.\n\t\t-d\t\tReturn the current deck of the player.\n\t\t-clan\t\tReturn the" + 
+	 " current clan of the player.\n\t\t-b\t\tReturn the total number of battles (including doubles).\n" + 
+	 "\n\t\tALIAS:\n\tplayer\n\n\tARGUMENT:\n\t\tplayer tag\t\tfound in player profile portal under the" + 
+	 "\n\t\t\t\t\tusername.\n")
+
+def print_clan_help():
+	# TODO: consolidate to one function
+	print("\nHelp: c [clan tag] [options...]\n\tPrints information about the clan. The clan tag is found under the clan profile.\n")
+	print("\tOPTIONS:")
+	print("\t\t-s\t\tReturn the clan score.")
+	print("\t\t-m\t\tReturn a list of members with their rank.")
+	print("\t\t-md\t\tReturn a list of members with donation amount.")
+	print("\t\t-mdr\t\tReturn a list of members with number of donations\n\t\t\t\trecieved.")
+	print("\t\t-td\t\tReturn the highest donor.")
+	print("\t\t-l\t\tReturn the leader.")
+	print("\n\tALIAS:\n\t\tclan\n\n\tARGUMENT:\n\t\tclan tag\t\tfound under the clan profile.\n")
+
+def print_help(arg):
+	if arg:
+		if arg == "p" or arg == "player":
+			print_player_help()
+		elif arg == "c" or arg == "clan":
+			print_clan_help()
+	else:
+		print('CLASH ROYALE REPL\nCommands:\n\th\t\tprints list of commands available.')
+		print('\tp\t\tprints player specific information. Enter "h p" for more information on the p command.')
+
+# def format_player_info(arg, api_key):
+# 	if 
+
+# compare statistics between players (if player hasn't been search for prior, needs to query api)
+# def compare_players(args, stat):
+# 	pass
+
+# compare stats between clans
+# def compare_clans(args, stat):
+# 	pass
+
 def parse(full_cmd, api_key):
 	cmd = full_cmd[0]
 	cmd.lower()
 	# will either be one (full information), or specify what info you want
 	arg = None
 	if len(full_cmd) > 1:
-		arg = full_cmd[1:]
-	# this should be either clan tag or player tag
-	arg.strip().upper()
-	if cmd == "p":
-		format_player_info(arg, api_key)
-	else if cmd == "c":
-		format_clan_info(arg, api_key)
-	else if cmd == "h" or 
+		args = full_cmd[1:]
+		# this should be either clan tag or player tag
+		for arg in args:
+			arg.strip().lower()
+	# if cmd == "p" or cmd == "player":
+		# loop through args again, if preceeded by -, lowercase 
+		# format_player_info(arg, api_key)
+	# elif cmd == "c" or cmd == "clan":
+		# format_clan_info(arg, api_key)
+	# elif cmd == "cmp" or "compare":
+	# need a way to distinguish between clan and player
+
+	if cmd == "h" or cmd == "help":
+		print_help(arg)
 	else:
 		print('Command not recognized. Enter "h" or "help" for a list of commands.')
 
 
 def repl(api_key):
+	# figure out why this creates two spaces
+	print('\n')
 	exit = True
+	# keep running list in current session for comparison
+	players = list()
+	clans = list()
 	while exit:
-		cmd = input(">> ")
+		cmd = input("Clash Royale>> ")
 		cmd.strip()
 		cmd = cmd.split()
 		if cmd is None or not len(cmd):
@@ -38,15 +93,13 @@ def repl(api_key):
 			if quit(cmd):
 				exit = False
 
-
-
-
 def main():
+	# TODO: ignore signals
 	print("\nEnter the file for your API Key.")
 	print('If you are using a preadded key, type "Default"')
-	api_key = input(">> ")
+	api_key = input("API file>> ")
 	api_key.lower().strip()
-	if api_key == "default":
+	if api_key == "default" or api_key == "d":
 		api_key = None
 	repl(api_key)
 
